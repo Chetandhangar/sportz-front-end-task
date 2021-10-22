c7492eeead6c0efa4eff911194b917ead81cfff8
Chetan Dhangar <chetansdhangar@gmail.com>
2021-10-22 13:50:41 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Front-End  Task
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+//yarn add history@5 react-router-dom@6
